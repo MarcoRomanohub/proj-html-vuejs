df262537d44d2d8b2682e74d571527d82a12f084
MarcoRomanoHub <marcoromano270@gmail.com>
2024-04-08 15:47:04 +0200

    [add] store.js and root palette color

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,7 @@ export default {
 
   <Header />
   <Main />
+  <Footer />
 
 </template>
 
--- src/data/store.js
+++ src/data/store.js
@@ -1,5 +1,34 @@
 import { reactive } from 'vue'
 
 export const store = reactive({
-
+  headerMenu: [
+    {
+      name: 'Home',
+      link: '#'
+    },
+    {
+      name: 'Courses',
+      link: '#'
+    },
+    {
+      name: 'About us',
+      link: '#'
+    },
+    {
+      name: 'News',
+      link: '#'
+    },
+    {
+      name: 'Pages',
+      link: '#'
+    },
+    {
+      name: 'Contact',
+      link: '#'
+    },
+    {
+      name: 'Purchase',
+      link: '#'
+    },
+  ]
 })
